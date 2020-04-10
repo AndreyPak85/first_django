8d89ff5b7ef17d6c339e10ac40a59ad3bc0b78d9
Andrey Pak <andrey.pak85@gmail.com>
2020-04-10 17:09:50 +0600

    10.04.2020 start day

--- bin/first/inst/admin.py
+++ bin/first/inst/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from inst.models import Inst, Position, Course
 
 # Register your models here.
+class InstAdmin(admin.ModelAdmin):
+    list_display = ["name", "surname"]
+    #fields = ["name", "surname", "email", "position"]
+
+    fieldsets = [(None, {'fields' : ["name", "surname"]}),]
+
+admin.site.register(Inst, InstAdmin)
+admin.site.register(Position)
+admin.site.register(Course)
--- bin/first/inst/models.py
+++ bin/first/inst/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Inst(models.Model):
-    name = models.CharField(max_length=65)
+    name = models.CharField(verbose_name=u'Instructor Name', max_length=65, help_text='This is name')
     surname = models.CharField(max_length=64)
-    course = models.CharField(max_length=64)
+    courses = models.ManyToManyField(Course)
+    date_bith = models.DateField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True)
+    is_active = models.BooleanField(default=True)
+    position = models.ForeignKey(Position, null=True)
+
+    def __str__(self):
+        return self.name + ' ' + self.surname
+
